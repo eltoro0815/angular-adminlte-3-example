0eb20019658bcdad88bf2ae8d9545bb16105ecd2
eltoro0815 <th_ru@web.de>
2023-03-13 19:26:49 +0100

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,47 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import {AppRoutingModule} from '@/app-routing.module';
-import {AppComponent} from './app.component';
-import {MainComponent} from '@modules/main/main.component';
-import {LoginComponent} from '@modules/login/login.component';
-import {HeaderComponent} from '@modules/main/header/header.component';
-import {FooterComponent} from '@modules/main/footer/footer.component';
-import {MenuSidebarComponent} from '@modules/main/menu-sidebar/menu-sidebar.component';
-import {BlankComponent} from '@pages/blank/blank.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {ProfileComponent} from '@pages/profile/profile.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RegisterComponent} from '@modules/register/register.component';
-import {DashboardComponent} from '@pages/dashboard/dashboard.component';
-import {ToastrModule} from 'ngx-toastr';
-import {MessagesComponent} from '@modules/main/header/messages/messages.component';
-import {NotificationsComponent} from '@modules/main/header/notifications/notifications.component';
+import { AppRoutingModule } from '@/app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from '@modules/main/main.component';
+import { LoginComponent } from '@modules/login/login.component';
+import { HeaderComponent } from '@modules/main/header/header.component';
+import { FooterComponent } from '@modules/main/footer/footer.component';
+import { MenuSidebarComponent } from '@modules/main/menu-sidebar/menu-sidebar.component';
+import { BlankComponent } from '@pages/blank/blank.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from '@pages/profile/profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from '@modules/register/register.component';
+import { DashboardComponent } from '@pages/dashboard/dashboard.component';
+import { ToastrModule } from 'ngx-toastr';
+import { MessagesComponent } from '@modules/main/header/messages/messages.component';
+import { NotificationsComponent } from '@modules/main/header/notifications/notifications.component';
 
-import {registerLocaleData} from '@angular/common';
+import { registerLocaleData } from '@angular/common';
 import localeEn from '@angular/common/locales/en';
-import {UserComponent} from '@modules/main/header/user/user.component';
-import {ForgotPasswordComponent} from '@modules/forgot-password/forgot-password.component';
-import {RecoverPasswordComponent} from '@modules/recover-password/recover-password.component';
-import {LanguageComponent} from '@modules/main/header/language/language.component';
-import {MainMenuComponent} from './pages/main-menu/main-menu.component';
-import {SubMenuComponent} from './pages/main-menu/sub-menu/sub-menu.component';
-import {MenuItemComponent} from './components/menu-item/menu-item.component';
-import {ControlSidebarComponent} from './modules/main/control-sidebar/control-sidebar.component';
-import {StoreModule} from '@ngrx/store';
-import {authReducer} from './store/auth/reducer';
-import {uiReducer} from './store/ui/reducer';
-import {ProfabricComponentsModule} from '@profabric/angular-components';
-import {defineCustomElements} from '@profabric/web-components/loader';
-import {SidebarSearchComponent} from './components/sidebar-search/sidebar-search.component';
+import localeDe from'@angular/common/locales/de';
+import { UserComponent } from '@modules/main/header/user/user.component';
+import { ForgotPasswordComponent } from '@modules/forgot-password/forgot-password.component';
+import { RecoverPasswordComponent } from '@modules/recover-password/recover-password.component';
+import { LanguageComponent } from '@modules/main/header/language/language.component';
+import { MainMenuComponent } from './pages/main-menu/main-menu.component';
+import { SubMenuComponent } from './pages/main-menu/sub-menu/sub-menu.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
+import { ControlSidebarComponent } from './modules/main/control-sidebar/control-sidebar.component';
+import { StoreModule } from '@ngrx/store';
+import { authReducer } from './store/auth/reducer';
+import { uiReducer } from './store/ui/reducer';
+import { ProfabricComponentsModule } from '@profabric/angular-components';
+import { defineCustomElements } from '@profabric/web-components/loader';
+import { SidebarSearchComponent } from './components/sidebar-search/sidebar-search.component';
 import { BookingsComponent } from './pages/bookings/bookings.component';
 
+import { EurPipe } from './pipes/eur.pipe';
+
 defineCustomElements();
-registerLocaleData(localeEn, 'en-EN');
+registerLocaleData(localeDe, 'de');
 
 @NgModule({
     declarations: [
@@ -63,11 +66,12 @@ registerLocaleData(localeEn, 'en-EN');
         MenuItemComponent,
         ControlSidebarComponent,
         SidebarSearchComponent,
-        BookingsComponent
+        BookingsComponent,
+        EurPipe
     ],
     imports: [
         BrowserModule,
-        StoreModule.forRoot({auth: authReducer, ui: uiReducer}),
+        StoreModule.forRoot({ auth: authReducer, ui: uiReducer }),
         HttpClientModule,
         AppRoutingModule,
         ReactiveFormsModule,
@@ -82,4 +86,4 @@ registerLocaleData(localeEn, 'en-EN');
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/pages/bookings/bookings.component.ts
+++ src/app/pages/bookings/bookings.component.ts
@@ -1,7 +1,33 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
     selector: 'app-bookings',
     templateUrl: './bookings.component.html',
     styleUrls: ['./bookings.component.scss']
 })
-export class BookingsComponent {}
+export class BookingsComponent {
+    data: any;
+    sum: any;
+    constructor(private http: HttpClient) {
+        this.http.get('http://backend.ddev.site/api/bookings').subscribe({
+            next: (response) => {
+                this.data = response['data'];
+
+                console.log(this.data);
+
+                this.sum = this.data.reduce(
+                    (acc, element) =>
+                        acc + parseFloat(element.amount)
+                    ,
+                0
+                );
+    },
+    error: (e) => {
+                console.error(e);
+},
+        });
+
+
+
+    }
+}
